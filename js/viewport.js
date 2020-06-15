b23443b56ba6ed116251e10a33bbe007bafa7549
sballesteros <tatanballesteros@hotmail.com>
2020-06-15 10:19:15 -0400

    Fixed log statement

--- js/viewport.js
+++ js/viewport.js
@@ -274,7 +274,7 @@ class ViewPort {
 
         // console.log('get features');
         try {
-            console.alert('getting features');
+            alert('getting features');
             const features = await this.getFeatures(this.trackView.track, referenceFrame.chrName, bpStart, bpEnd, referenceFrame.bpPerPixel);
             let roiFeatures = [];
             const roi = mergeArrays(this.browser.roi, this.trackView.track.roi)
